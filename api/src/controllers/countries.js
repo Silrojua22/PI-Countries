78452e11a3f74b75700c5d93a1c806d9d35e6a39
Silrojua22 <117102050+Silrojua22@users.noreply.github.com>
2023-06-06 15:44:12 -0300

    Deploy back

--- api/src/controllers/countries.js
+++ api/src/controllers/countries.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const { Country, Activity } = require('../db');
 const { Op } = require('sequelize');
-
+require("dotenv").config();
+const { DB_URL } = process.env;
 const getAllCountries = async (req, res) => {
   try {
     // Obtener el parámetro 'name' de la consulta (query)
@@ -12,7 +13,7 @@ const getAllCountries = async (req, res) => {
 
     if (!countryCount) {
       // Si no hay países en la base de datos, obtener los países de la API externa
-      const response = await axios.get('https://restcountries.com/v3/all');
+      const response = await axios.get(DB_URL);
       const apiCountries = response.data;
 
       // Mapear los datos de los países obtenidos de la API a un formato deseado
@@ -57,31 +58,31 @@ const getAllCountries = async (req, res) => {
 };
 
 const getCountriesId = async (req, res) => {
-    const { id } = req.params;
-    try {
-      // Busca el país por su ID en la base de datos
-      const country = await Country.findByPk(id.toUpperCase(), {
-        include: {
-          model: Activity,
-          through: { attributes: [] },
-        },
-      });
-  
-      // Verifica si se encontró el país
-      if (country) {
-        // Devuelve el país encontrado en la respuesta con un código de estado 200 (OK)
-        res.status(200).json(country);
-      } else {
-        // Si no se encontró el país, devuelve un mensaje de error con un código de estado 404 (Not Found)
-        res.status(404).json({ error: { message: 'El país no se encontró', values: { id } } });
-      }
-    } catch (error) {
-      console.error('Error al obtener el país por ID:', error);
-      // Si ocurre algún error durante la búsqueda, devuelve un mensaje de error con un código de estado 500 (Internal Server Error)
-      res.status(500).json({ error: 'Error al obtener el país por ID' });
+  const { id } = req.params;
+  try {
+    // Busca el país por su ID en la base de datos
+    const country = await Country.findByPk(id.toUpperCase(), {
+      include: {
+        model: Activity,
+        through: { attributes: [] },
+      },
+    });
+
+    // Verifica si se encontró el país
+    if (country) {
+      // Devuelve el país encontrado en la respuesta con un código de estado 200 (OK)
+      res.status(200).json(country);
+    } else {
+      // Si no se encontró el país, devuelve un mensaje de error con un código de estado 404 (Not Found)
+      res.status(404).json({ error: { message: 'El país no se encontró', values: { id } } });
     }
-  };
-  
+  } catch (error) {
+    console.error('Error al obtener el país por ID:', error);
+    // Si ocurre algún error durante la búsqueda, devuelve un mensaje de error con un código de estado 500 (Internal Server Error)
+    res.status(500).json({ error: 'Error al obtener el país por ID' });
+  }
+};
+
 
 module.exports = {
   getAllCountries,
